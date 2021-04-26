d6b76551bf1063b955665696af1db782908dde84
Bilal Hussain <bilal_hussain@college.harvard.edu>
2021-04-26 02:12:27 -0400

    Update refs.ml

--- refs.ml
+++ refs.ml
@@ -13,6 +13,34 @@ Problem 1: Write a function has_cycle that returns whether a mutable
 list has a cycle. You may want a recursive helper function. Don't
 worry about space usage.
 ......................................................................*)
+
+let rtn_cycle mlst = 
+    let rec find_cycle mls ls =
+        (match mls with 
+        | Nil -> None
+        | Cons (_, tl) ->
+            match !tl with 
+            | Nil -> None
+            | Cons (_, xd) -> if (xd==Nil) then None
+            | Cons (_, y_ls) -> 
+            if mls == y_ls then Some tl
+            else (find_cycle tl (mls))) in
+    find_cycle mlst mlst;;
+
+let has_cycle (ls: 'a mlist) : bool =  
+    match ls with 
+    | Nil -> false
+    | Cons (_, rt) -> if (find_cycle = None) then false else true
+
+let flatten (lst: 'a mlist)  : unit =
+    match lst with 
+    | Nil -> false
+    | Cons (_, tail) -> if (has_cycle tail) then 
+    match rtn_cycle lst lst with
+    | None -> ()
+    | Some t -> t:= Nil
+    
+OR
                                       
 let has_cycle (ls: 'a mlist) : bool = 
   let rec check (ls1: 'a mlist) (ls2: 'a mlist) (ls2_ref: ('a mlist) ref) : bool =
