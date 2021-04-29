6d0a83039dfe430d1ae62698671776bbbe28b2d8
Bilal Hussain <bilal_hussain@college.harvard.edu>
2021-04-28 21:12:14 -0400

    Update refs.ml

--- refs.ml
+++ refs.ml
@@ -23,7 +23,7 @@ let rtn_cycle mlst =
             | Nil -> None
             | Cons (_, xd) -> if (xd==Nil) then None
             | Cons (_, y_ls) -> 
-            if mls == y_ls then Some tl
+            if (mls == y_ls) then Some tl
             else (find_cycle tl (mls))) in
     find_cycle mlst mlst;;
 
@@ -32,14 +32,7 @@ let has_cycle (ls: 'a mlist) : bool =
     | Nil -> false
     | Cons (_, rt) -> if (find_cycle = None) then false else true
 
-let flatten (lst: 'a mlist)  : unit =
-    match lst with 
-    | Nil -> false
-    | Cons (_, tail) -> if (has_cycle tail) then 
-    match rtn_cycle lst lst with
-    | None -> ()
-    | Some t -> t:= Nil
-    
+   
 OR
                                       
 let has_cycle (ls: 'a mlist) : bool = 
@@ -49,7 +42,7 @@ let has_cycle (ls: 'a mlist) : bool =
      |Cons (_, tl) -> if (!tl == Nil) then false else
            (match !tl with
             |Cons (_, y_ls) -> 
-                if ((y_ls != Nil) && (ls2_ref == y_ls)) then true
+                if ((y_ls !== Nil) && (!ls2_ref == y_ls)) then true
                 else check tl ls2 (!y_ls || has_cycle ls2)) in
   check ls ls (ref ls);;
   
@@ -72,7 +65,13 @@ let has_cycle (ls: 'a mlist) : bool =
     match ls with 
     | Nil -> false
     | Cons (_, rt) -> if (find_cycle !ls ls = None) then false else true
+    
 
+(*......................................................................
+Problem 2: Write a function flatten that flattens a list (removes its
+cycles if it has any) destructively. Again, you may want a recursive
+helper function and you shouldn't worry about space.
+......................................................................*)
 let flatten (lst: 'a mlist)  : unit =
     match lst with 
     | Nil -> false
@@ -80,7 +79,11 @@ let flatten (lst: 'a mlist)  : unit =
     match rtn_cycle lst lst with
     | None -> ()
     | Some t -> t:= Nil
-
+ 
+(*......................................................................
+Problem 3: Write mlength, which nondestructively finds the number of
+nodes in a mutable list that may have cycles.
+......................................................................*)
 let rec helper_length mlst n  = 
     match mlst with 
     | Nil -> n
@@ -91,23 +94,15 @@ let rec helper_length mlst n  =
             match !rest with
             | Nil -> n+2
             | Cons (_, rest') -> 
-                    if !rest' == Nil then n+2 else
+                    if (!rest' == Nil) then n+2 else
                     helper_length rest n+2;;
-
-(*......................................................................
-Problem 2: Write a function flatten that flattens a list (removes its
-cycles if it has any) destructively. Again, you may want a recursive
-helper function and you shouldn't worry about space.
-......................................................................*)
-let flatten (lst : 'a mlist) : unit =
-  failwith "flatten not implemented"
-
-(*......................................................................
-Problem 3: Write mlength, which nondestructively finds the number of
-nodes in a mutable list that may have cycles.
-......................................................................*)
-let mlength (lst : 'a mlist) : int =
-  failwith "mlength not implemented"
+                    
+let m_length (lst: 'a mlist) : int = 
+    let lst_cpy = lst in
+    if (has_cycle lst) then flatten lst_cpy 
+    match lst_cpy with
+    | Nil -> 0
+    | Cons (_, tl) -> helper_length tl 0;;
                          
 (*======================================================================
 Time estimate
